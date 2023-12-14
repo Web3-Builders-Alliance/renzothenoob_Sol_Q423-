f3173e3096156cf2d32fb8f286bb7c6c1407894f
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-14 15:02:23 +0800

    Update config

--- cluster1/config/index.ts
+++ cluster1/config/index.ts
@@ -3,18 +3,34 @@ import {
   Commitment,
   Connection,
   Keypair,
+  PublicKey,
   clusterApiUrl,
 } from "@solana/web3.js";
 import { CLUSTER, PRIVATE_KEY } from "../env";
 import base58 from "bs58";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { Umi, createSignerFromKeypair } from "@metaplex-foundation/umi";
 
-const COMMITMENT: Commitment = "confirmed";
+const COMMITMENT: Commitment = "finalized";
+
+export const CLUSTER_API_URL: string = clusterApiUrl(CLUSTER as Cluster);
 
 export const CONNECTION: Connection = new Connection(
-  clusterApiUrl(CLUSTER as Cluster),
+  CLUSTER_API_URL,
   COMMITMENT
 );
 
 export const DECODED_KEYPAIR: Uint8Array = base58.decode(PRIVATE_KEY);
 
 export const WBA_KEYPAIR: Keypair = Keypair.fromSecretKey(DECODED_KEYPAIR);
+
+export const UMI: Umi = createUmi(CLUSTER_API_URL);
+
+export const UMI_KEYPAIR =
+  UMI.eddsa.createKeypairFromSecretKey(DECODED_KEYPAIR);
+
+export const UMI_SIGNER_KEYPAIR = createSignerFromKeypair(UMI, UMI_KEYPAIR);
+
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
